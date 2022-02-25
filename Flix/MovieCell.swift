82ae657bd79ea33e2e89307bcf7ecc21b098304f
Ngan Nguyen <hieungan5b@gmail.com>
2022-02-25 08:03:39 -0600

    first commit

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Hieu Ngan Nguyen on 2/24/22.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
