2c1ccaff78341fcd8673d3906ade989f759865e5
Ngan Nguyen <hieungan5b@gmail.com>
2022-03-02 17:32:02 -0600

    fix comments

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -17,25 +17,33 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
-        // Do any additional setup after loading the view.
+        
+        /* Create a URL: unwrap the optional URL with a !
+        in case not a valid URL -> app crash to fix it */
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        // create the URLRequest object - information about the request
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        // create the session object
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        // send request to the server
         let task = session.dataTask(with: request) { (data, response, error) in
              // This will run when the network request returns
              if let error = error {
                     print(error.localizedDescription)
              } else if let data = data {
+                 // Get the array of movies
+                 /* Deserialize the JSON Data to a generic object
+                  -> Downcast the resulting obj to a dict */
                     let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                 self.movies = dataDictionary["results"] as! [[String:Any]]
-                 self.tableView.reloadData()
                     print(dataDictionary)
-                    // TODO: Get the array of movies
-                    // TODO: Store the movies in a property to use elsewhere
-                    // TODO: Reload your table view data
+                     // Store the movies as an array of dictionaries
+                     self.movies = dataDictionary["results"] as! [[String:Any]]
+                     // Reload your table view data
+                     self.tableView.reloadData()
 
              }
         }
+        // Tasks are created in a suspended state. To initiate the task, you must call it’s resume() method.
         task.resume()
     }
     
